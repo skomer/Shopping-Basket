a11b285edba430f685ba74b89f6d00782ed8ee8e
Josephine Elder <parusmontanus@gmail.com>
2016-11-21 12:52:24 +0000

    Initial commit

---
+++ Item.java
@@ -0,0 +1,20 @@
+
+public class Item() {
+
+    String name;
+    double basePrice;
+    boolean isBogof;
+
+    public void Item(String name, double basePrice, boolean isBogof) {
+        this.name = name;
+        this.basePrice = basePrice;
+        this.isBogof = isBogof;
+    }
+
+
+    
+
+
+
+
+}
