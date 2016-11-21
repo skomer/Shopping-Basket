6c2140b19dbe626b4a1ea728c61db54aa4d7ce69
Josephine Elder <parusmontanus@gmail.com>
2016-11-21 13:27:49 +0000

    Set up Item test file

--- ItemTest.java
+++ ItemTest.java
@@ -1,3 +1,36 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+public class ItemTest {
+
+    Item lightbulb = new Item("lightbulb", 4.99, false);
+    Item smellyCandle = new Item("scented candle", 8.99, true);
+    Item peanut = new Item("peanut", 0.05, false);
+    Item frozenPizza = new Item("frozen pizza", 3.00, false);
+
+    @Test
+    public void hasName() {
+
+    }
+
+    @Test
+    public void hasBasePrice() {
+
+    }
+
+    @Test
+    public void isBogofOrNot() {
+        
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
